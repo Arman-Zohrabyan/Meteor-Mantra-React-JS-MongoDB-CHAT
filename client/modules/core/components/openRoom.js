904c1950cd1cc93a781a42e8d97618f7b2a00fc0
Arman-Zohrabyan <arman.zohrabyan.96@gmail.com>
2017-11-03 08:12:26 +0400

    added form

--- client/modules/core/components/openRoom.js
+++ client/modules/core/components/openRoom.js
@@ -1,6 +1,29 @@
 import React from 'react';
 
+import t from 'tcomb-form';
+const Form = t.form.Form;
+
+import RoomForm from '/lib/roomForm';
+
+
 export default class OpenRoom extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formValue: RoomForm.getDefaultValues(),
+      formType: RoomForm.getType()
+    }
+  }
+
+  createNewRoom() {
+    const value = this.refs.roomForm.getValue();
+    if(!values) {
+      console.warn('Validation Error.');
+    } else {
+      
+    }
+  }
+
   render() {
     return (
       <div id="rooms">
@@ -10,10 +33,15 @@ export default class OpenRoom extends React.Component {
               <h4 className="flex-center">Open New Room</h4>
               <div className="row">
                 <div className="col-xs-10 col-xs-offset-1">
+                  <Form
+                    ref="roomForm"
+                    type={this.state.formType}
+                    value={this.state.formValue}
+                  />
                 </div>
               </div>
               <div className="flex-center padding-vertical-md">
-                <div className="btn-c btn-20 margin-horizontal-md">Open</div>
+                <div className="btn-c btn-20 margin-horizontal-md onClick={this.createNewRoom.bind(this)}">Open</div>
                 <div className="btn-c btn-20 margin-horizontal-md" onClick={() => FlowRouter.go('chat.rooms')}>Back</div>
               </div>
             </div>
